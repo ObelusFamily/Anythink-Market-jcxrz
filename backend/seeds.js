7e9c3c77997b6f6082bce34a98a0fa6af4ba16cc
eitangrunseid <eitangrunseid@gmail.com>
2022-04-10 15:32:50 +0300

    only items

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,36 +1,8 @@
 var axios = require("axios");
 
-// let config = {
-//   method: "post",
-//   url: "http://localhost:3000/api/users",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "X-Requested-With": "XMLHttpRequest",
-//   },
-//   data: {
-//     user: {
-//       email: "john@jacob.com",
-//       password: "12345678",
-//       username: "johnjacob",
-//     },
-//   },
-// };
-
-// for (let i = 0; i < 101; i++) {
-//   config.data.user.email = `john${i}@jacob.com`;
-//   config.data.user.username = `johnjacob${i}`;
-
-//   axios(config)
-//     .then(function (response) {
-//       console.log(JSON.stringify(response.data));
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// }
 
 
-const addItem = (token) => {
+
 
     var data = JSON.stringify({
       "item": {
@@ -50,7 +22,7 @@ const addItem = (token) => {
       headers: { 
         'Content-Type': 'application/json', 
         'X-Requested-With': 'XMLHttpRequest', 
-        'Authorization': `Token ${token}`
+        'Authorization': `Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTJjMTRhYWIzZDI4MDA0MTk1MDY2OCIsInVzZXJuYW1lIjoiam9obmphY29iMTAwIiwiZXhwIjoxNjU0Nzc0NjAzLCJpYXQiOjE2NDk1OTA2MDN9.RRaRbTItSQTH-MZCZrYXd2IQCxw1qMd-oi7UmJbT404`
       },
       data : data
     };
@@ -63,7 +35,5 @@ const addItem = (token) => {
       console.log(error);
     });
     
-};
 
-addItem('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTJjMTRhYWIzZDI4MDA0MTk1MDY2OCIsInVzZXJuYW1lIjoiam9obmphY29iMTAwIiwiZXhwIjoxNjU0Nzc0NjAzLCJpYXQiOjE2NDk1OTA2MDN9.RRaRbTItSQTH-MZCZrYXd2IQCxw1qMd-oi7UmJbT404')
 
