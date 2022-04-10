db49f3e5f84e3316df53b336540fb9dc4b22745f
eitangrunseid <eitangrunseid@gmail.com>
2022-04-10 13:20:38 +0300

    added seeds.js file uploading an item to DB

---
+++ backend/seeds.js
@@ -0,0 +1,31 @@
+var axios = require('axios');
+var data = JSON.stringify({
+  "item": {
+    "title": "How to train your dragon",
+    "description": "Ever wonder how?",
+    "body": "Very carefully.",
+    "tagList": [
+      "dragons",
+      "training"
+    ]
+  }
+});
+
+var config = {
+  method: 'post',
+  url: 'http://localhost:3000/api/items',
+  headers: { 
+    'Content-Type': 'application/json', 
+    'X-Requested-With': 'XMLHttpRequest', 
+    'Authorization': 'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGYyMzAyOTQ4NTI5MDA0ODQ0YmYyZCIsInVzZXJuYW1lIjoiZWl0YW5ncnVuc2VpZCIsImV4cCI6MTY1NDYwNjQ4OSwiaWF0IjoxNjQ5NDIyNDg5fQ.9WWPAYK0gzbp8TN8UBGMeDbrSUb01sqh1jnG6pbfGqY'
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
