7677070b54da9ea0dbb122d6038d1d2e10d6bd20
eitangrunseid <eitangrunseid@gmail.com>
2022-04-10 20:59:36 +0300

    Yall hapoel

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,7 +1,7 @@
 var axios = require('axios');
 
 
-for(let i = 0; i < 102; i++) {
+for(let i = 0; i < 101; i++) {
 
     config = {
 
@@ -32,33 +32,33 @@ for(let i = 0; i < 102; i++) {
 }
 
 
-// var data = JSON.stringify({
-//   "item": {
-//     "title": "How to train your dragon",
-//     "description": "Ever wonder how?",
-//     "body": "Very carefully.",
-//     "tagList": [
-//       "dragons",
-//       "training"
-//     ]
-//   }
-// });
+var data = JSON.stringify({
+  "item": {
+    "title": "Yalla Hapoel!",
+    "description": "Ever wonder how?",
+    "body": "Very carefully.",
+    "tagList": [
+      "dragons",
+      "training"
+    ]
+  }
+});
 
-// var config = {
-//   method: 'post',
-//   url: 'http://localhost:3000/api/items',
-//   headers: { 
-//     'Content-Type': 'application/json', 
-//     'X-Requested-With': 'XMLHttpRequest', 
-//     'Authorization': 'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGYyMzAyOTQ4NTI5MDA0ODQ0YmYyZCIsInVzZXJuYW1lIjoiZWl0YW5ncnVuc2VpZCIsImV4cCI6MTY1NDYwNjQ4OSwiaWF0IjoxNjQ5NDIyNDg5fQ.9WWPAYK0gzbp8TN8UBGMeDbrSUb01sqh1jnG6pbfGqY'
-//   },
-//   data : data
-// };
+var config1 = {
+  method: 'post',
+  url: 'http://localhost:3000/api/items',
+  headers: { 
+    'Content-Type': 'application/json', 
+    'X-Requested-With': 'XMLHttpRequest', 
+    'Authorization': 'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGYyMzAyOTQ4NTI5MDA0ODQ0YmYyZCIsInVzZXJuYW1lIjoiZWl0YW5ncnVuc2VpZCIsImV4cCI6MTY1NDYwNjQ4OSwiaWF0IjoxNjQ5NDIyNDg5fQ.9WWPAYK0gzbp8TN8UBGMeDbrSUb01sqh1jnG6pbfGqY'
+  },
+  data : data
+};
 
-// axios(config)
-// .then(function (response) {
-//   console.log(JSON.stringify(response.data));
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
+axios(config1)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
