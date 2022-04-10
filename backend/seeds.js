566b9a4391379410ab7afb318b2ec7b4b55a602d
eitangrunseid <eitangrunseid@gmail.com>
2022-04-10 21:04:05 +0300

    Yalla hapoel 1

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,35 +1,35 @@
 var axios = require('axios');
 
 
-for(let i = 0; i < 101; i++) {
+// for(let i = 0; i < 101; i++) {
 
-    config = {
+//     config = {
 
-        method: 'POST',
-        url: 'http://localhost:3000/api/users',
-        headers: { 
-            'Content-Type': 'application/json', 
-            'X-Requested-With': 'XMLHttpRequest', 
-        },
-        data: {
-          user: {
-            email: `${i}john@jacob.com`,
-            password: "1234678",
-            username: `${i}john`
-          }
-        }
-    }
+//         method: 'POST',
+//         url: 'http://localhost:3000/api/users',
+//         headers: { 
+//             'Content-Type': 'application/json', 
+//             'X-Requested-With': 'XMLHttpRequest', 
+//         },
+//         data: {
+//           user: {
+//             email: `${i}john@jacob.com`,
+//             password: "1234678",
+//             username: `${i}john`
+//           }
+//         }
+//     }
 
-    axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+//     axios(config)
+// .then(function (response) {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
         
       
-}
+// }
 
 
 var data = JSON.stringify({
